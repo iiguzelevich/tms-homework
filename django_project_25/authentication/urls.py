0d714b0965ddaab298851a232411a351c6666819
iiguzelevich <iiguzelevich@gmail.com>
2022-12-25 17:17:27 +0300

    added a hw_30

--- django_project_25/authentication/urls.py
+++ django_project_25/authentication/urls.py
@@ -1,14 +1,26 @@
 from django.urls import path
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+from django.urls import include
+from django.urls import re_path
+
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+from .views import UserPofileCreateView
+from .views import UserPofileDetailView
 
 urlpatterns = [
+    re_path(r'^all-profile/', UserPofileCreateView.as_view()),
+    re_path(r'^profile/<int:pk>', UserPofileDetailView.as_view()),
+    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    re_path(r'^api/auth/', include('djoser.urls')),
     path(
         'api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'
-    ),
+         ),
     path(
         'api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'
+    ),
+    path(
+        'api/token/refresh/', TokenVerifyView.as_view(), name='token_refresh'
     ),
 ]
--- django_project_25/config/settings.py
+++ django_project_25/config/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+from datetime import timedelta
+
 
 load_dotenv()
 
@@ -28,7 +30,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
@@ -43,7 +45,10 @@ INSTALLED_APPS = [
     'appdrf.apps.AppdrfConfig',
     'authentication.apps.AuthenticationConfig',
     'rest_framework',
+    'rest_framework.authtoken',
+    'djoser',
     'rest_framework_simplejwt',
+
 ]
 
 MIDDLEWARE = [
@@ -140,7 +145,42 @@ REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
         'rest_framework.renderers.BrowsableAPIRenderer',
-    ]
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    )
 }
 
-AUTH_USER_MODEL = 'authentication.User'
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
+    'JWK_URL': None,
+    'LEEWAY': 0,
+
+    'AUTH_HEADER_TYPES': ('Bearer',),
+    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'USER_AUTHENTICATION_RULE': 'rest_framework_simplejwt.authentication.default_user_authentication_rule',
+
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'TOKEN_USER_CLASS': 'rest_framework_simplejwt.models.TokenUser',
+
+    'JTI_CLAIM': 'jti',
+
+    'SLIDING_TOKEN_REFRESH_EXP_CLAIM': 'refresh_exp',
+    'SLIDING_TOKEN_LIFETIME': timedelta(minutes=5),
+    'SLIDING_TOKEN_REFRESH_LIFETIME': timedelta(days=1),
+}
